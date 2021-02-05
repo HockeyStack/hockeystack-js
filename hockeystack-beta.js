4e19561c0ea76099c243906bc270696ee0c1ba66
ardaa627 <arda627211@gmail.com>
2021-02-06 00:33:07 +0300

    improved tracking in beta

--- hockeystack-beta.js
+++ hockeystack-beta.js
@@ -1,127 +1,78 @@
-(function (root, factory) {
-  if(typeof define === 'function' && define.amd) { // AMD
-    root.HockeyStack = factory(root.HockeyStack);
-  } else if(typeof module === 'object' && module.exports) { // Node
-    module.exports = factory(root.HockeyStack);
-  } else { // Browser globals
-    root.HockeyStack = factory(root.HockeyStack);
-  }
-}(typeof window !== 'undefined' ? window : this, function (HockeyStack) {
-  'use strict';
-
-  HockeyStack = HockeyStack || {};
+window.HockeyStack = (() => {
+  'use strict'
+  const HockeyStack = {};
 
   // VARIABLES
   const serverURL = 'https://hockeystack.com';
-  const inactivityTime = 5;
+  const inactivityTime = 10;
+  const eventListenerList = [];
+  const actionLog = [];
   let inactivityCounter = 0;
   let isInactive = false;
   let scrollMax = 0;
-  let eventListenerList = [];
   let lastActiveDate = new Date();
   let isDevicePhone;
-  let browser;
+  let globalCurrentPage = window.location.href;
 
   const documentAlias = document;
   const navigatorAlias = navigator;
   const windowAlias = window;
 
-  // INITIALIZE PLUGIN
   HockeyStack.init = function (apiKey) {
+    history.replaceState(null, null, document.URL);
+
     if (apiKey !== null) HockeyStack.apiKey = apiKey;
     if (isBot()) return; // if bot, don't run the script
 
-    browser = getBrowser();
     lastActiveDate = new Date();
     isDevicePhone = getDevice(navigatorAlias.userAgent.toLowerCase()) === 'phone';
 
-    if (!localStorage.getItem('hs_eq')) HockeyStack.startSession();
-    else HockeyStack.trackSession();
-  };
-
-  // HOCKEYSTACK METHODS
-  HockeyStack.serialize = (value) => {
-    if (typeof value === 'object' || typeof value === 'number') {
-      value = JSON.stringify(value);
-    }
-    return value;
-  };
-
-  HockeyStack.startSession = () => {
-    isInactive = false;
-    addAction('start-session');
+    createAction('enter-page', globalCurrentPage);
+    setTimeout(sendActions, 750); // concurrency issues
 
     // check if revisiting user
     if (!getCookie('hs_uuid')) setCookie('hs_uuid', getId(), 365);
 
-    sendReqViaBeacon(serverURL + '/data/send');
     HockeyStack.trackSession();
   };
 
   HockeyStack.endSession = function () {
-    if (!localStorage.getItem('hs_eq')) return;
-
     let scrollDepth = (!hasVerticalScroll()) ? 100 : parseInt((scrollMax / (getDocHeight() - getViewportHeight())) * 100);
     if (scrollDepth > 100) scrollDepth = 100;
 
     if (isInactive) {
-      addAction('scroll-depth', null, scrollDepth);
-      addAction('end-session');
-
-      sendReqViaBeacon(serverURL + '/data/send');
-
-      removeLocalStorageItems();
+      createAction('scroll-depth', null, scrollDepth);
+      createAction('end-session');
+      sendActions('beacon');
       return;
     }
 
     let nextSiteURL = documentAlias.activeElement.href;
-    if (sameSite(nextSiteURL)) {
+    if (sameSite(nextSiteURL) && globalCurrentPage !== nextSiteURL) {
       nextSiteURL = nextSiteURL.split('#')[0];
-      const lastURL = JSON.parse(localStorage.getItem('hs_eq')).slice(-1)[0].url;
       const currentURL = windowAlias.location.origin + windowAlias.location.pathname;
-      if (nextSiteURL.startsWith('http') && currentURL !== nextSiteURL && currentURL === lastURL) {
-        addAction('scroll-depth', null, scrollDepth);
-        addAction('exit-page');
-        addAction('enter-page', nextSiteURL);
+      if (nextSiteURL.startsWith('http') && currentURL !== nextSiteURL) { // && currentURL === lastURL
+        createAction('scroll-depth', null, scrollDepth);
+        createAction('exit-page');
+        sendActions('beacon');
       }
     } else {
-      addAction('scroll-depth', null, scrollDepth);
-      addAction('end-session');
-
-      sendReqViaBeacon(serverURL + '/data/send');
-
-      removeLocalStorageItems();
+      createAction('scroll-depth', null, scrollDepth);
+      createAction('end-session');
+      sendActions('beacon');
     }
   };
 
   HockeyStack.trackSession = () => {
-    const lastDateFromQueue = new Date(JSON.parse(localStorage.getItem('hs_eq')).splice(-1)[0].actions.splice(-1)[0].actionDate);
-    if ((new Date().getTime()) - lastDateFromQueue.getTime() > (1000 * 60 * inactivityTime)) {
-      isInactive = true;
-      lastActiveDate = lastDateFromQueue;
+    isInactive = false;
 
-      const scrollDepth = JSON.parse(localStorage.getItem('hs_sdkey')) * 10; // if there still isn't a scroll-depth value, returns 0
-
-      const lastURLFromQueue = JSON.parse(localStorage.getItem('hs_eq')).splice(-1)[0].url;
-      addAction('scroll-depth', lastURLFromQueue, scrollDepth);
-      addAction('end-session', lastURLFromQueue);
-
-      sendReqViaBeacon(serverURL + '/data/send');
-
-      removeLocalStorageItems();
-
-      isInactive = false;
-      inactivityCounter = 0;
-      HockeyStack.init(HockeyStack.apiKey);
-    }
+    addEvent(windowAlias, 'pagehide', () => {
+      HockeyStack.endSession();
+    });
 
     if (isDevicePhone) {
-      addEvent(windowAlias, 'pagehide', () => {
-        HockeyStack.endSession();
-      });
-    } else {
-      addEvent(windowAlias, 'beforeunload', () => {
-        HockeyStack.endSession();
+      addEvent(windowAlias, 'pageshow', (event) => {
+        if (event.persisted) HockeyStack.init(HockeyStack.apiKey);
       });
     }
 
@@ -152,16 +103,15 @@
       addEvent(windowAlias, 'focus', () => {
         resetInactivity();
       });
-      addEvent(windowAlias, 'pageshow', () => {
-        resetInactivity();
-      });
     };
 
     function onClickFunc (e) {
+      if (!window.innerPageClick) return;
       resetInactivity();
 
       const obj = getClickInfo(e.target);
-      addAction('onclick', null, obj);
+      createAction('onclick', null, obj);
+      urlRefresh();
     };
 
     let scrollHeight = 0;
@@ -170,17 +120,15 @@
       resetInactivity();
 
       scrollHeight = Math.max(scrollHeight, window.scrollY, document.body.scrollTop, document.documentElement.scrollTop);
-      if (scrollHeight > scrollMax) {
-        scrollMax = scrollHeight;
-
-        if (isDevicePhone && browser === 'Safari') {
-          let scrollDepth = (!hasVerticalScroll()) ? 100 : parseInt((scrollMax / (getDocHeight() - getViewportHeight())) * 100);
-          if (scrollDepth > 100) scrollDepth = 100;
-          const localScrollDepth = JSON.parse(localStorage.getItem('hs_sdkey'));
-          if (parseInt(scrollDepth / 10) > localScrollDepth) localStorage.setItem('hs_sdkey', parseInt(scrollDepth / 10))
-        }
-      }
+      if (scrollHeight > scrollMax) scrollMax = scrollHeight;
     };
+
+    addEvent(document, 'mouseover', () => {
+      window.innerPageClick = true;
+    });
+    addEvent(document, 'mouseleave', () => {
+      window.innerPageClick = false;
+    });
 
     function resetInactivity () {
       if ((new Date().getTime()) - lastActiveDate.getTime() > (1000 * 60 * inactivityTime)) {
@@ -189,11 +137,11 @@
       }
       lastActiveDate = new Date();
       inactivityCounter = 0;
-      isInactive = false;
 
-      if (localStorage.getItem('hs_eq') === null) {
+      if (isInactive) {
         HockeyStack.init(HockeyStack.apiKey);
       }
+      isInactive = false;
     };
 
     addEvent(windowAlias, 'mousemove', resetInactivity);
@@ -214,6 +162,14 @@
     }
   };
 
+  // HOCKEYSTACK METHODS
+  HockeyStack.serialize = (value) => {
+    if (typeof value === 'object' || typeof value === 'number') {
+      value = JSON.stringify(value);
+    }
+    return value;
+  }
+
   // OTHER METHODS
   const addEvent = (element, type, listener) => {
     if (eventListenerList.includes(type)) return;
@@ -222,38 +178,57 @@
     eventListenerList.push(type);
   };
 
-  const addAction = (actionType, URL, actionInfo) => {
-    let actionObject = {
+  const urlRefresh = () => {
+    globalCurrentPage = window.location.href;
+  }
+
+  const createAction = (actionType, URL, actionInfo) => {
+    const actionObject = {
       actionType: actionType,
-      actionDate: (isInactive) ? new Date(lastActiveDate.getTime()).toISOString() : new Date().toISOString(),
+      actionDate: (isInactive) ? new Date(lastActiveDate.getTime()).toISOString() : new Date().toISOString()
     };
+
+    const currentURL = URL || (windowAlias.location.origin + windowAlias.location.pathname);
+    const localCurrentPage = globalCurrentPage;
 
     if (actionType === 'onclick') {
       actionObject.actionElement = actionInfo.element;
       if (actionInfo.text) actionObject.actionText = actionInfo.text;
-      if (actionInfo.url) actionObject.actionURL = actionInfo.url;
-    } else if (actionType === 'scroll-depth') actionObject.actionNumber = actionInfo;
-
-    const currentURL = (!URL) ? (windowAlias.location.origin + windowAlias.location.pathname): URL;
-    const queue = JSON.parse(localStorage.getItem('hs_eq')) || [];
-    const len = queue.length;
-
-    if (len === 0 || queue[len - 1].url !== currentURL) {
-      let pagesObject = {
-        url: currentURL,
-        actions: [actionObject]
+      if (actionInfo.url) {
+        actionObject.actionURL = actionInfo.url;
+        if (actionInfo.url === currentURL && currentURL !== localCurrentPage) { // for spa
+          createAction('onclick', localCurrentPage, actionInfo);
+          const scrollDepth = (!hasVerticalScroll()) ? 100 : parseInt((scrollMax / (getDocHeight() - getViewportHeight())) * 100);
+          createAction('scroll-depth', localCurrentPage, scrollDepth);
+          createAction('exit-page', localCurrentPage);
+          createAction('enter-page', actionInfo.url);
+          sendActions();
+          addEvent(window, 'popstate', () => {
+            if (window.innerPageClick) {
+              // your own in-page
+            } else {
+              // Browser back button was clicked
+              createAction('onclick', globalCurrentPage, actionInfo);
+              const scrollDepth = (!hasVerticalScroll()) ? 100 : parseInt((scrollMax / (getDocHeight() - getViewportHeight())) * 100);
+              createAction('scroll-depth', globalCurrentPage, scrollDepth);
+              createAction('exit-page', globalCurrentPage);
+              createAction('enter-page', windowAlias.location.origin + windowAlias.location.pathname);
+              sendActions();
+              globalCurrentPage = windowAlias.location.origin + windowAlias.location.pathname;
+            }
+          });
+          return;
+        }
       }
-      queue.push(pagesObject);
-    } else {
-      queue[len - 1].actions.push(actionObject);
-    }
-    localStorage.setItem('hs_eq', HockeyStack.serialize(queue));
+    } else if (actionType === 'scroll-depth') actionObject.actionNumber = actionInfo;
+    actionLog.push({ action: actionObject, url: currentURL });
   };
 
-  const removeLocalStorageItems = () => {
-    localStorage.removeItem('hs_eq');
-    localStorage.removeItem('hs_sdkey');
-  };
+  const sendActions = (type) => {
+    if (type === 'beacon') sendReqViaBeacon();
+    else sendReqViaXHR();
+    actionLog.length = 0;
+  }
 
   const supportsSendBeacon = () => {
     return typeof navigatorAlias === 'object' &&
@@ -261,19 +236,19 @@
       typeof Blob === 'function';
   };
 
-  const sendReqViaBeacon = (url) => {
+  const sendReqViaBeacon = () => {
     const isSupported = supportsSendBeacon();
 
     if (!isSupported) return false;
+    if (actionLog.length === 0) return false;
 
     const customerObject = {
       website: window.location.hostname,
       apiKey: HockeyStack.apiKey
     };
     const userObject = getUserProperties();
-    const sessionObject = localStorage.getItem('hs_eq');
     const referrer = document.referrer;
-    const requestObj = { customerObject, userObject, sessionObject, referrer };
+    const requestObj = { customerObject, userObject, actionLog, referrer };
 
     const headers = { type: 'application/x-www-form-urlencoded; charset=UTF-8' };
     let success = false;
@@ -281,18 +256,38 @@
     try {
       const blob = new Blob([JSON.stringify(requestObj, null, 2)], headers);
 
-      success = navigatorAlias.sendBeacon(url, blob);
+      success = navigatorAlias.sendBeacon(serverURL + '/data/send-beta', blob);
     } catch (e) {
       return false;
     }
-
     return success;
+  };
+
+  const sendReqViaXHR = () => {
+    if (actionLog.length === 0) return false;
+
+    const customerObject = {
+      website: window.location.hostname,
+      apiKey: HockeyStack.apiKey
+    };
+    const userObject = getUserProperties();
+    const referrer = document.referrer;
+    const requestObj = { customerObject, userObject, actionLog, referrer };
+
+    const request = new XMLHttpRequest();
+    request.open('POST', serverURL + '/data/send-beta', true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.send(JSON.stringify(requestObj));
+
+    request.onreadystatechange = () => {
+      return true;
+    }
   };
 
   const sameSite = (uri) => {
     if (typeof uri === 'undefined') return false;
 
-    const matches = uri.match(/^(https?:)?\/\/([^\/]+)/);
+    const matches = uri.match(/^(https?:)?\/\/([^/]+)/);
     if (!matches) return true;
 
     return windowAlias.location.host === matches[2];
@@ -315,7 +310,7 @@
   const setCookie = (cookieKey, value, days) => {
     const date = new Date();
     date.setDate(date.getDate() + days);
-    const cookieValue = value + ((days == null) ? '' : '; expires=' + date.toUTCString()) + '; secure; path=/';
+    const cookieValue = value + ((days == null) ? '' : '; expires=' + date.toUTCString()) + '; path=/;SameSite=Lax';
     documentAlias.cookie = cookieKey + '=' + cookieValue;
   };
 
@@ -342,47 +337,14 @@
 
   const getDevice = (userAgent) => {
     return /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent) ? 'tablet'
-              : /(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds|archos|skyfire|puffin|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard)/.test(userAgent) ? 'phone'
-              : 'desktop';
-  };
-
-  const getBrowser = () => {
-    const ua = navigatorAlias.userAgent.toLowerCase();
-    let browser;
-
-    if ((ua.indexOf('opera') !== -1 || ua.indexOf('opr')) !== -1) browser = 'Opera';
-    else if (ua.indexOf('chrome') !== -1 || ua.indexOf('crios') !== -1) browser = 'Chrome';
-    else if (ua.indexOf('firefox') !== -1) browser = 'Firefox';
-    else if (ua.indexOf('safari') !== -1) browser = 'Safari';
-    else if ((ua.indexOf('msie') !== -1) || (!!documentAlias.documentMode)) browser = 'IE';
-    else browser = 'unknown';
-
-    return browser;
-  };
-
-  const getOS = () => {
-    let platform = navigatorAlias.platform;
-    if (!platform) return 'Android';
-    platform = platform.toLowerCase();
-    if (platform.startsWith('ip')) return 'IOS';
-    else if (platform.startsWith('win')) return 'Windows';
-    else if (platform.startsWith('mac')) return 'Mac';
-    else if (platform.startsWith('playstation') || platform.startsWith('psp')) return 'Sony';
-    else if (/Android/.test(navigatorAlias.userAgent)) return 'Android';
-    else if (platform.startsWith('linux')) return 'Linux';
-    else return 'unknown'
+      : /(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds|archos|skyfire|puffin|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard)/.test(userAgent) ? 'phone'
+        : 'desktop';
   };
 
   const getUserProperties = () => {
-    const userAgent = navigatorAlias.userAgent.toLowerCase();
-
     return {
-      browser: getBrowser(),
-      os: getOS(),
-      device: getDevice(userAgent),
-      cookieEnabled: navigatorAlias.cookieEnabled,
       language: navigatorAlias.language,
-      location: '',
+      platform: navigatorAlias.platform,
       uuid: getId()
     }
   };
@@ -416,7 +378,7 @@
   };
 
   const getClickInfo = el => {
-    let path = [];
+    const path = [];
     let text = '';
     let url = '';
 
@@ -440,7 +402,7 @@
         break;
       } else {
         const isSVG = (el.nodeType === 1 && el.namespaceURI === 'http://www.w3.org/2000/svg');
-        let className = (isSVG ? (el.className ? el.className.baseVal : el.getAttribute('class')) : el.className).trim() || '';
+        const className = (isSVG ? (el.className ? el.className.baseVal : el.getAttribute('class')) : el.className).trim() || '';
         if (className !== '') t += '.' + className.replace(/ /g, '.');
 
         const parentElement = el.parentElement;
@@ -465,6 +427,5 @@
     return res;
   };
 
-  // RETURN OBJECT
   return HockeyStack;
-}));
+})();
